6153c5a28ad7fb77600ac2271e11168541963405
MinhNguyenn204 <110523059+MinhNguyenn204@users.noreply.github.com>
2022-08-04 15:02:47 +0700

    B.Minh Nộp Bài Tập

---
+++ Cau1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+	if(a>0){
+		printf("Xd la so duong", a);
+	
+	}else{
+		printf("%d la so khong duong", a);
+	}	
+}
---
+++ Cau2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	printf("nhap num=");
+	scanf("%d",&num);
+	if(num>0)
+	printf("%d la so duong", num);
+	else
+	if (num<0)
+	printf("%d la so am", num);
+	else
+	printf("%d bang 0" , num);
+	}
+
+	
